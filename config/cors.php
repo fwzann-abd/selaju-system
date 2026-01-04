04200dcf2cd9a1eda071897c13cb15364276a84a
AgungPajar <presetpjr@gmail.com>
2026-01-04 15:43:01 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,15 +19,10 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:8080',
-        'http://localhost:5173',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:3001',
-        'http://127.0.0.1:8080',
-        'http://127.0.0.1:5173',
         'https://selaju.dev',
         'https://www.selaju.dev',
+        'https://selangkahlebihmaju.web.id',
+        'https://www.selangkahlebihmaju.web.id',
     ],
 
     'allowed_origins_patterns' => [],
